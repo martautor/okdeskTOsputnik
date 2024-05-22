b2eed229e1642f9d2be7a913cd8ff77fa1655c32
martautor <167963978+martautor@users.noreply.github.com>
2024-05-22 13:35:50 +0300

    fixed ENV vars, version 5.0.0 (stable)

--- client/src/fetching.js
+++ client/src/fetching.js
@@ -1,6 +1,6 @@
 // import {useState, useEffect} from 'react'
 export default async function Fetching(fID, lID) {
-     return fetch(`http://192.168.0.9:5000/api/start/?firstId=${fID}&lastId=${lID}`, {
+     return fetch(`http://${process.env.fullAddress}/api/start/?firstId=${fID}&lastId=${lID}`, {
       method: "POST", 
       headers: {
         "Content-Type": "application/json"
